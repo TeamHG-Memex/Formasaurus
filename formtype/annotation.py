9dc920530836ad375865e1d82bfb01ff85869132
Mikhail Korobov <kmike84@gmail.com>
2014-12-11 09:10:30 +0500

    CLI for training and classification

---
+++ formtype/annotation.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+HTML forms interactive annotation utilities.
+"""
+from __future__ import absolute_import
+import sys
+import os
+import urllib2
+
+import lxml.html
+from lxml.html.clean import Cleaner
+
+from formtype.storage import Storage, FORM_TYPES, load_html, FORM_TYPES_INV
+
+
+def annotate_forms(data_folder, url_argument):
+    """
+    Run an interactive HTML form annotation tool.
+
+    The process is to download a web page, display all HTML forms and for
+    each form ask user about form type. The result is saved on disk:
+    web page is stored as a html file and the URL and the annotation
+    results are added to index.json file.
+    """
+    storage = Storage(data_folder)
+    html, url = load_data(url_argument)
+    doc = load_html(html, url)
+    answers = _annotate_forms(storage, doc)
+    if answers:
+        storage.store_result(html, answers, url)
+
+
+def check_annotated_data(data_folder):
+    """
+    Check that annotated data is correct; exit with code 1 if it is not.
+    """
+    storage = Storage(data_folder)
+    ok = storage.check()
+    storage.print_type_counts()
+    if not ok:
+        sys.exit(1)
+
+
+def load_data(url_or_path):
+    """
+    Load binary data from a local file or an url;
+    return (data, url) tuple.
+    """
+    if os.path.exists(url_or_path):
+        raise NotImplementedError("Re-annotation is not supported yet")
+        # with open(url_or_path, 'rb') as f:
+        #     return f.read(), None
+    else:
+        return urllib2.urlopen(url_or_path).read(), url_or_path
+
+
+def print_form_html(form):
+    """ Print a cleaned up version of <form> HTML contents """
+    cleaner = Cleaner(
+        forms=False,
+        javascript=True,
+        scripts=True,
+        style=True,
+        allow_tags={'form', 'input', 'textarea', 'label', 'option', 'select', 'submit', 'a'},
+
+        remove_unknown_tags=False,
+    )
+    html = cleaner.clean_html(lxml.html.tostring(form, pretty_print=True))
+    lines = [line.strip() for line in html.splitlines(False) if line.strip()]
+    print("\n".join(lines))
+
+
+def print_form_types(types):
+    print("\nAllowed form types and their shortcuts:")
+    for full_name, shortcuts in types.items():
+        print("  %s %s" % (shortcuts, full_name))
+    print("")
+
+
+def _annotate_forms(storage, doc, form_types=None):
+    """
+    For each form element ask user whether it is a login form or not.
+    Return an array with True/False answers.
+    """
+    forms = doc.xpath("//form")
+    if not forms:
+        print("Page has no forms.")
+        return []
+    else:
+        print("Page has %d form(s)" % len(forms))
+
+    fingerprints = storage.get_fingerprints()
+
+    if form_types is None:
+        form_types = FORM_TYPES
+
+    print_form_types(form_types)
+    shortcuts = "/".join(form_types.values())
+
+    res = []
+    for idx, form in enumerate(forms, 1):
+
+        fp = storage.get_fingerprint(form)
+        if fp in fingerprints:
+            xpath = "//form[%d]" % idx
+            tp = FORM_TYPES_INV[fingerprints[fp]]
+            print("Skipping duplicate form %-10s %r" % (xpath, tp))
+            res.append("X")
+            continue
+
+        print_form_html(form)
+
+        while True:
+            tp = raw_input("\nPlease enter the form type (%s) or ? for help: " % shortcuts).strip()
+            if tp == '?':
+                print_form_types(form_types)
+                continue
+            if tp not in set(shortcuts):
+                print("Please enter one of the following letters: %s. You entered %r" % (shortcuts, tp))
+                continue
+            res.append(tp)
+            break
+
+        print("="*40)
+
+    if all(r == 'X' for r in res):
+        print("Page has no new forms.")
+        return []
+    return res
--- formtype/tool.py
+++ formtype/tool.py
@@ -1,143 +1,94 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-HTML form annotation tool.
+This is a tool for annotating HTML forms, training a classifier
+based on annotated data and evaluating its quality.
 
+To annotate new pages use "add" command.
 It downloads a web page, displays all HTML forms and for each form asks
 user about form type. The result is saved on disk: web page is stored
 as a html file and the URL and the annotation results are added
 to index.json file.
 
-To annotate new pages use "forms" command; to check the storage for
-consistency and print some stats use "check" command.
+To check the storage for consistency and print some stats use "check" command.
+
+To train an extractor for HTML form classification use "train" command.
+
+To classify forms from an URL using a saved extractor use "run" command.
+
 
 Usage:
-    tool.py forms <url> [--data-folder <path>]
-    tool.py check [--data-folder <path>]
+    tool.py run <modelfile> <url> [--threshold <probability>]
+    tool.py add <url> [--data-folder <path>]
+    tool.py check-data [--data-folder <path>]
+    tool.py train <modelfile> [--data-folder <path>]
+    tool.py evaluate [--test-size <ratio>] [--cv <n_folds>] [--data-folder <path>]
     tool.py -h | --help
     tool.py --version
 
 Options:
-    --data-folder <path>   path to the data folder [default: data]
+    --data-folder <path>       path to the data folder [default: data]
+    --test-size <ratio>        ratio of data to use for evaluation, from 0 to 1.0 [default: 0.3]
+    --cv <n_folds>             use <n_folds> for cross-validation [default: 10]
+    --threshold <probability>  don't display predictions with probability below this threshold [default: 0.01]
 
 """
-from __future__ import absolute_import
-import sys
-import os
-import urllib2
+from __future__ import absolute_import, print_function
+from collections import Counter
 
 import docopt
-import lxml.html
-from lxml.html.clean import Cleaner
 
-from formtype.storage import Storage, FORM_TYPES, load_html, FORM_TYPES_INV
-
-
-def load_data(url_or_path):
-    """
-    Load binary data from a local file or an url;
-    return (data, url) tuple.
-    """
-    if os.path.exists(url_or_path):
-        raise NotImplementedError("Re-annotation is not supported yet")
-        # with open(url_or_path, 'rb') as f:
-        #     return f.read(), None
-    else:
-        return urllib2.urlopen(url_or_path).read(), url_or_path
-
-
-def print_form_html(form):
-    """ Print a cleaned up version of <form> HTML contents """
-    cleaner = Cleaner(
-        forms=False,
-        javascript=True,
-        scripts=True,
-        style=True,
-        allow_tags={'form', 'input', 'textarea', 'label', 'option', 'select', 'submit', 'a'},
-
-        remove_unknown_tags=False,
-    )
-    html = cleaner.clean_html(lxml.html.tostring(form, pretty_print=True))
-    lines = [line.strip() for line in html.splitlines(False) if line.strip()]
-    print("\n".join(lines))
-
-
-def print_form_types(types):
-    print("\nAllowed form types and their shortcuts:")
-    for full_name, shortcuts in types.items():
-        print("  %s %s" % (shortcuts, full_name))
-    print("")
-
-
-def annotate_forms(storage, doc, form_types=None):
-    """
-    For each form element ask user whether it is a login form or not.
-    Return an array with True/False answers.
-    """
-    forms = doc.xpath("//form")
-    if not forms:
-        print("Page has no forms.")
-        return []
-    else:
-        print("Page has %d form(s)" % len(forms))
-
-    fingerprints = storage.get_fingerprints()
-
-    if form_types is None:
-        form_types = FORM_TYPES
-
-    print_form_types(form_types)
-    shortcuts = "/".join(form_types.values())
-
-    res = []
-    for idx, form in enumerate(forms, 1):
-
-        fp = storage.get_fingerprint(form)
-        if fp in fingerprints:
-            xpath = "//form[%d]" % idx
-            tp = FORM_TYPES_INV[fingerprints[fp]]
-            print("Skipping duplicate form %-10s %r" % (xpath, tp))
-            res.append("X")
-            continue
-
-        print_form_html(form)
-
-        while True:
-            tp = raw_input("\nPlease enter the form type (%s) or ? for help: " % shortcuts).strip()
-            if tp == '?':
-                print_form_types(form_types)
-                continue
-            if tp not in set(shortcuts):
-                print("Please enter one of the following letters: %s. You entered %r" % (shortcuts, tp))
-                continue
-            res.append(tp)
-            break
-
-        print("="*40)
-
-    if all(r == 'X' for r in res):
-        print("Page has no new forms.")
-        return []
-    return res
+from formtype.annotation import (
+    annotate_forms,
+    check_annotated_data,
+    load_data,
+    print_form_html
+)
+from formtype.extractor import FormExtractor
+from formtype.storage import load_html, FORM_TYPES_INV
 
 
 def main():
     args = docopt.docopt(__doc__)
 
-    if args['forms']:
-        storage = Storage(args['--data-folder'])
-        html, url = load_data(args["<url>"])
-        doc = load_html(html, url)
-        answers = annotate_forms(storage, doc)
-        if answers:
-            storage.store_result(html, answers, url)
+    if args['add']:
+        annotate_forms(
+            data_folder=args['--data-folder'],
+            url_argument=args["<url>"],
+        )
 
-    elif args['check']:
-        storage = Storage(args['--data-folder'])
-        ok = storage.check()
-        storage.print_type_counts()
-        if not ok:
-            sys.exit(1)
+    elif args['check-data']:
+        check_annotated_data(args['--data-folder'])
+
+    elif args['train']:
+        ex = FormExtractor.trained_on(
+            data_folder=args["--data-folder"],
+            train_ratio=float(args["--test-size"])
+        )
+        ex.save(args["<modelfile>"])
+
+    elif args['run']:
+        threshold = float(args['--threshold'])
+        print("Loading the extractor..")
+        ex = FormExtractor.load(args["<modelfile>"])
+        print("Downloading data...")
+        data, url = load_data(args["<url>"])
+        tree = load_html(data, url)
+
+        result = ex.extract_forms_proba(tree, threshold)
+        if not result:
+            print("No forms found.")
+            return
+
+        for form, probs in result:
+            print("-"*40)
+            print_form_html(form)
+            print("")
+            for tp, prob in Counter(probs).most_common():
+                tp_full = FORM_TYPES_INV[tp]
+                print("%s %0.1f%%" % (tp_full, prob*100), end='    ')
+
+            print("")
 
 
 if __name__ == '__main__':
